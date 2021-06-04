7710a9338e5cdfc1e009ba789c6c4fba0ce3325f
Samuel <samkoj@hotmail.co.uk>
2021-06-04 17:01:57 +0100

    create note component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const currentYear = new Date().getFullYear();
+
+export default function Footer() {
+	return (
+		<footer>
+			<p>Copyright &copy; {currentYear}</p>
+		</footer>
+	);
+}
